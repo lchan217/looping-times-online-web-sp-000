cc3d4b30d45eaea07029e129926ab62cf358af80
Lizzie Chan <lchan217@gmail.com>
2019-01-25 14:47:46 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -2,7 +2,9 @@ def using_times
 	#your code here
 end
 
-7.times do
-puts "I am doing the dishes left by my former friends."
+jewels_in_bag = 100
+ 
+3.times do 
+  puts "Hiding 10 stolen jewels."
+  jewels_in_bag = jewels_in_bag - 10
 end
-
